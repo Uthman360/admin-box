3bcdf1dd1a2b31437046036f734c65637c558fd7
unknown <uthman1227@gmail.com>
2025-02-27 16:03:45 +0500

    issue resolve

--- app/helpers/Sidebar.jsx
+++ app/helpers/Sidebar.jsx
@@ -1,11 +1,13 @@
 "use client";
-import React from "react";
-import { usePathname } from "next/navigation"; 
+import React, { useState } from "react";
+import { usePathname } from "next/navigation";
 import Link from "next/link";
 import "./helper.css";
 
 const Sidebar = () => {
   const pathname = usePathname(); // Get the current path
+  const [isOpen, setIsOpen] = useState(false);
+
   const menuItems = [
     { name: "Dashboard", path: "/dashboard" },
     { name: "Users", path: "/users" },
@@ -13,19 +15,29 @@ const Sidebar = () => {
     { name: "Sub Admins", path: "/sub-admins" },
     { name: "Guidelines", path: "/guidelines" },
     { name: "Push Notifications", path: "/push-notifications" },
-    { name: "Support", path: "/support" }
+    { name: "Support", path: "/support" },
   ];
 
+  const ToggleHandler = () => {
+    setIsOpen(!isOpen)
+  }
+
   return (
-    <div className='left_dash'>
-      <ul>
-        {menuItems.map((item, index) => (
-         <Link href={item.path} key={index}>  <li  className={pathname === item.path ? "active" : ""}>
-           {item.name}
-          </li></Link>
-        ))}
-      </ul>
-    </div>
+    <>
+      <button className='menu-toggle' onClick={ToggleHandler}>☰</button>
+      <div className={`left_dash ${isOpen ? "open" : ""}`} >
+        <ul>
+          {menuItems.map((item, index) => (
+            <Link href={item.path} key={index}>
+              {" "}
+              <li className={pathname === item.path ? "active" : ""}>
+                {item.name}
+              </li>
+            </Link>
+          ))}
+        </ul>
+      </div>
+    </>
   );
 };
 
